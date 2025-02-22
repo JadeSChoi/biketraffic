d4df2cbf81372a64d2d1c3cc28feed5230948de7
JadeSChoi <130327976+JadeSChoi@users.noreply.github.com>
2025-02-21 20:16:26 -0800

    Update map.js

--- map.js
+++ map.js
@@ -15,7 +15,7 @@ map.on('load', () => {
     //code 
   map.addSource('boston_route', {
     type: 'geojson',
-    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson?...'
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson'
   });
   
   map.addLayer({
